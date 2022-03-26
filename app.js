8c7c9f3ecb373d8e59e709c3bafc3e70b04a8ffc
juhan00 <jvabya@gmail.com>
2022-03-26 23:48:46 +0900

    Feat: add pagination function

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
+const store = {
+  currentPage: 1,
+  limitPage: 10,
+};
+
 function getData(url) {
   ajax.open("GET", url, false);
   ajax.send();
@@ -16,13 +21,15 @@ function newsFeed() {
   const newsList = [];
 
   newsList.push("<ul>");
-
-  for (let i = 0; i < 10; i++) {
-    const div = document.createElement("div");
-
+  console.log(newsFeed.length);
+  for (
+    let i = (store.currentPage - 1) * store.limitPage;
+    i < store.currentPage * store.limitPage;
+    i++
+  ) {
     newsList.push(`
     <li>
-      <a href="#${newsFeed[i].id}">
+      <a href="#/show/${newsFeed[i].id}">
         ${newsFeed[i].title} (${newsFeed[i].comments_count})
       </a>
     </li>
@@ -30,7 +37,18 @@ function newsFeed() {
   }
 
   newsList.push("</ul>");
-
+  newsList.push(`
+    <div>
+      <a href="#/page/${
+        store.currentPage > 1 ? store.currentPage - 1 : 1
+      }">이전 페이지</a>
+      <a href="#/page/${
+        store.currentPage < newsFeed.length / store.limitPage
+          ? store.currentPage + 1
+          : store.currentPage
+      }">다음 페이지</a>
+    </div>
+  `);
   container.innerHTML = newsList.join("");
 }
 
@@ -43,7 +61,7 @@ function newsDetail() {
     <h1>${newsContent.title}</h1>
 
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `;
 }
@@ -52,6 +70,9 @@ function router() {
   const routePath = location.hash;
 
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substr(7));
     newsFeed();
   } else {
     newsDetail();
