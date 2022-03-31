3f7fccf66b26500722876be7c122f636c5fe4dd5
juhan00 <jvabya@gmail.com>
2022-03-31 16:42:26 +0900

    Feat: add tailwindcss/font-awesome, apply design

--- app.js
+++ app.js
@@ -20,16 +20,28 @@ function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
   let template = `
-    <div>
-      <h1>Hacker News</h1>
-      <ul>
-        {{__news_feed__}}
-      </ul>
-      <div>
-        <a href="#/page/{{__prev_page__}}">이전 페이지</a>
-        <a href="#/page/{{__next_page__}}">다음 페이지</a>
+  <div class="bg-gray-600 min-h-screen">
+    <div class="bg-white text-xl">
+      <div class="mx-auto p-4">
+        <div class="flex justify-between items-center py-6">
+          <div class="flex justify-start">
+            <h1 class="font-extrabold">Hacker News</h1>
+          </div>
+          <div class="items-center justify-end">
+            <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+              Previous
+            </a>
+            <a href="#/page/{{__prev_page__}}" class="text-gray-500">
+              Previous
+            </a>
+          </div>
+        </div>
       </div>
     </div>
+    <div class="p-4 text-2xl text-gray-700">
+      {{__news_feed__}}
+    </div>
+  </div>
   `;
 
   for (
@@ -38,11 +50,23 @@ function newsFeed() {
     i++
   ) {
     newsList.push(`
-    <li>
-      <a href="#/show/${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-      </a>
-    </li>
+    <div class="p-6 bg-white mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+      <div class="flex">
+        <div class="flex-auto">
+          <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>
+        </div>
+        <div class="text-center text-sm">
+          <div class="w-10 text-white bg-green-300 rounded-lg px-0 py-2">${newsFeed[i].comments_count}</div>
+        </div>
+      </div>
+      <div class="flex mt-3">
+        <div class="grid grid-cols-3 text-sm text-gray-500">
+          <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
+          <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+          <div><i class="fas fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
+        </div>
+      </div>
+    </div>
   `);
   }
 
@@ -62,17 +86,38 @@ function newsFeed() {
 }
 
 function newsDetail() {
-  const id = location.hash.substr(1);
-
+  const id = location.hash.replace("#/show/", "");
   const newsContent = getData(CONTENT_URL.replace("@id", id));
 
-  container.innerHTML = `
-    <h1>${newsContent.title}</h1>
-
-    <div>
-      <a href="#/page/${store.currentPage}">목록으로</a>
+  let template = `
+  <div class="bg-gray-600 min-h-screen pb-8">
+    <div class="bg-white text-xl">
+      <div class="mx-auto px-4">
+        <div class="flex justify-between items-center py-6">
+          <div class="flex justify-start">
+            <h1 class="font-extrabold">Hacker News</h1>
+          </div>
+          <div class="items-center justify-end">
+            <a href="#/page/${store.currentPage}" class="text-gray-500">
+              <i class="fa fa-times"></i>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
+
+    <div class="h-full border rounded-xl bg-white m-6 p-4">
+      <h2>${newsContent.title}</h2>
+      <div class="text-gray-400 h-20">
+        ${newsContent.content}
+      </div>
+
+      {{__comments__}}
+    
+    </div>
+  </div>
   `;
+  container.innerHTML = template;
 }
 
 function router() {
@@ -87,6 +132,7 @@ function router() {
     newsDetail();
   }
 }
+
 window.addEventListener("hashchange", router);
 
 router();
